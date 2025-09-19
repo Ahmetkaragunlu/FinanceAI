c43729f1409567ec30dbd3bb900ba63a9c4f40ec
Ahmet Karagunlu <ahmetkaragunlu67@gmail.com>
2025-09-19 19:20:37 +0300

    App module changed

--- app/src/main/java/com/ahmetkaragunlu/financeai/di/AppModule.kt
+++ app/src/main/java/com/ahmetkaragunlu/financeai/di/AppModule.kt
@@ -1,5 +1,7 @@
 package com.ahmetkaragunlu.financeai.di
 
+import com.ahmetkaragunlu.financeai.firebaseRepo.AuthRepository
+import com.ahmetkaragunlu.financeai.firebaseRepo.AuthRepositoryImpl
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -12,15 +14,25 @@ import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-object AppModule {
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseStore(): FirebaseFirestore = FirebaseFirestore.getInstance()
 
     @Provides
     @Singleton
-    fun provideFirebaseStore() : FirebaseFirestore = FirebaseFirestore.getInstance()
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
     @Provides
     @Singleton
-    fun provideFirebaseAuth() : FirebaseAuth = FirebaseAuth.getInstance()
+    fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RepositoryModule {
     @Provides
     @Singleton
-    fun provideFirebaseStorage() : FirebaseStorage = FirebaseStorage.getInstance()
+    fun provideAuthRepository(auth: FirebaseAuth, firestore: FirebaseFirestore): AuthRepository =
+        AuthRepositoryImpl(auth, firestore)
 }
